e352d611f16eef18ceeb1f8f83a07d12005801a5
Michael Sledge <michael@sledgend.net>
2015-11-24 12:34:26 +0000

    fix header to match  c

--- src/netio.h
+++ src/netio.h
@@ -12,7 +12,7 @@ struct net_server{
 };
 
 
-int net_create_server(struct net_server *srv);
+struct net_server* net_create_server();
 
 static void listen_evt_cb(struct evconnlistener *listener, evutil_socket_t fd,
         struct sockaddr *addr, int socklen, void *arg);
