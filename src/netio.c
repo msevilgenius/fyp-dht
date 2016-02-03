650c96bf09d5cee50ae8c606a25086b108da0d21
Michael Sledge <msevilgenius@users.noreply.github.com>
2016-02-03 21:56:24 +0000

    implemented creating/using outgoing connections better

--- src/netio.c
+++ src/netio.c
@@ -49,6 +49,9 @@ struct net_server* net_server_create(uint16_t port, bufferevent_data_cb msg_hand
 void net_server_destroy(struct net_server* srv)
 {
     if(srv){
+        for(int i = 0; i < MAX_OUTGOING_CONNS; ++i){
+            net_close_connection(srv, i);
+        }
         evconnlistener_disable(srv->listener_evt);
         event_base_loopbreak(srv->base);
         evconnlistener_free(srv->listener_evt);
@@ -89,35 +92,49 @@ int net_server_run(struct net_server* srv)
     return 0;
 }
 
-int net_connect_to(struct net_server* srv, uint32_t IP, uint16_t port)
+int net_create_connection(struct net_server* srv, uint32_t IP, uint16_t port)
 {
-    struct sockaddr_in sin;
+    int conn = -1;
+    for(int i = 0; i < MAX_OUTGOING_CONNS; ++i){
+        if (srv->out_conns[i].bev == NULL){
+            conn = i;
+            break;
+        }
+    }
+    if (conn == -1){
+        return -1; }
 
-    memset(&sin, 0, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_addr.s_addr = htonl(IP);
-    sin.sin_port = htons(port);
-    // TODO save connection to outcons
+    memset(srv->out_conns[conn].sin, 0, sizeof(srv->out_conns[conn].sin));
+    srv->out_conns[conn].sin.sin_family = AF_INET;
+    srv->out_conns[conn].sin.sin_addr.s_addr = htonl(IP);
+    srv->out_conns[conn].sin.sin_port = htons(port);
 
-    return -1;
+
+    struct event_base* base = srv->base;
+
+    srv->out_conns[conn].bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
+
+    return conn;
+}
+
+void net_close_connection(struct net_server* srv, int connection)
+{
+    struct bufferevent *bev = srv->out_conns[connection].bev;
+    bufferevent_free(bev);
 }
 
 int net_send_message(struct net_server* srv, char* message, int connection,
                      bufferevent_data_cb reply_handler, void* rh_arg)
 {
-    struct event_base* base = srv->base;
-    struct bufferevent *bev;
-
-    bev = bufferevent_socket_new(base, -1, BEV_OPT_CLOSE_ON_FREE);
-
-    // TODO get connection instead from outconns
+    struct event_base *base = srv->base;
+    struct bufferevent *bev = srv->out_conns[connection].bev;
+    struct sockaddr *sin = &(srv->out_conns[connection].sin);
 
     bufferevent_setcb(bev, reply_handler, NULL, NULL, rh_arg);
     bufferevent_enable(bev, EV_READ|EV_WRITE);
     evbuffer_add_printf(bufferevent_get_output(bev), "%s", message);
 
-    if (bufferevent_socket_connect(bev,
-            (struct sockaddr *)&sin, sizeof(sin)) < 0) {
+    if (bufferevent_socket_connect(bev, sin, sizeof(sin)) < 0) {
         bufferevent_free(bev);
         return -1;
     }
--- src/netio.h
+++ src/netio.h
@@ -9,11 +9,15 @@
 
 #define MAX_OUTGOING_CONNS 64
 
+struct outcon{
+    struct bufferevent* bev;
+    struct sockaddr_in sin;
+};
 
 struct net_server{
     struct event_base *base;
     struct evconnlistener *listener_evt;
-    struct bufferevent* out_conns[MAX_OUTGOING_CONNS];
+    struct outcon out_conns[MAX_OUTGOING_CONNS];
     bufferevent_data_cb incoming_handler;
     void* incoming_handler_arg;
 };
@@ -29,7 +33,12 @@ int net_server_run(struct net_server* srv);
  * initialise a connection to another node/server
  * returns a connection number to use with net_send_message or negative number on error
  */
-int net_connect_to(struct net_server* srv, uint32_t IP, uint16_t port);
+int net_create_connection(struct net_server* srv, uint32_t IP, uint16_t port);
+
+/*
+ * close a connection to another node/server
+ */
+void net_close_connection(struct net_server* srv, int connection);
 
 /*
  * send a message through a connection
