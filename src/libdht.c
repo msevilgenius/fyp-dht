61265b76ec2c6c3bdc60dd1d1b4cbe0dc0228b6f
Michael Sledge <msevilgenius@users.noreply.github.com>
2016-03-03 19:47:40 +0000

    decide on hash func

--- src/libdht.c
+++ src/libdht.c
@@ -25,17 +25,3 @@ int two_to_the_n(int n)
     if (n < 1) return 1;
     return 2 << (n-1);
 }
-
-hash_type get_id(const char* name)
-{
-    unsigned char sha_1[SHA_DIGEST_LENGTH];
-    hash_type hash_id = 0;
-
-    SHA1((unsigned char *)name, strlen(name), sha_1);
-    int bytes = sizeof(hash_type);
-    for (int i = 0; i < bytes; ++i){
-        hash_id += ((hash_type)sha_1[i]) << 8*(bytes-(i+1)); //uff...
-    }
-
-    return hash_id;
-}
