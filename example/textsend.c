3e009c0bb995f22887c21d6b3ec2002f47cdaefd
Michael Sledge <michael@sledgend.net>
2016-04-13 15:14:05 +0100

    finished extsend

--- example/textsend.c
+++ example/textsend.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <pthread.h>
+#include <stdint.h>
+#include <string.h>
 #include <signal.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 
 #include <event2/event.h>
@@ -22,7 +22,7 @@ pthread_t inthr;
 
 void read_in_msg(struct node_self* node, struct node_message* msg, int conn, void *arg)
 {
-    printf("msg->len: %d\n", msg->len);
+    //printf("msg->len: %d\n", msg->len);
     struct evbuffer* rbuf = net_connection_get_read_buffer(net, conn);
     //printf("buffer len: %lu\n", evbuffer_get_length(rbuf));
     //uint32_t read = 0;
@@ -40,16 +40,6 @@ void read_in_msg(struct node_self* node, struct node_message* msg, int conn, voi
     evbuffer_drain(rbuf, -1);
 }
 
-
-void read_incoming(int conn, void *arg)
-{
-    char msg[1024];
-    struct evbuffer* rbuf = net_connection_get_read_buffer(net, conn);
-    evbuffer_remove(rbuf, msg, 1023);
-    msg[1023] = '\0';
-    printf("%s\n", msg);
-}
-
 void event_incoming(int conn, short what, void *arg)
 {
     if (what & (BEV_ERROR|BEV_EVENT_EOF|BEV_EVENT_TIMEOUT)){
@@ -57,15 +47,7 @@ void event_incoming(int conn, short what, void *arg)
     }
 }
 
-void incoming(int conn, short what, void *arg)
-{
-    net_connection_set_read_cb(net, conn, read_incoming);
-    net_connection_set_event_cb(net, conn, event_incoming);
-    struct timeval tmo = {5,0};
-    net_connection_set_timeouts(net, conn, &tmo, &tmo);
 
-    printf("incoming accepted\n");
-}
 
 void out_conn_event(int connection, short type, void *arg)
 {
@@ -76,9 +58,9 @@ void out_conn_event(int connection, short type, void *arg)
 
 void found_node(struct node_info ninfo, void* arg)
 {
-    printf("found node?\n");
+    //printf("found node?\n");
     char *msg = (char*) arg;
-    printf("sending [%lu] %s\n", strlen(msg), msg);
+    //printf("sending [%lu] %s\n", strlen(msg), msg);
     struct node_message nmsg;
     nmsg.to = ninfo;
     nmsg.content = msg;
@@ -110,14 +92,14 @@ void *in_thread(void *arg)
         while (!fgets(name, 256, stdin));
         printf("enter message\n");
         while (!fgets(msg, 1024, stdin));
-        printf("sending %s\n", msg);
+        //printf("sending %s\n", msg);
         node_find_successor(node, get_id(name), found_node, (void*)msg);
     }
 }
 
 void net_joined(void* arg)
 {
-    printf("joined_cb\n");
+    printf("Joined Network\n");
     pthread_create(&inthr, NULL, in_thread, NULL);
 }
 
@@ -125,23 +107,22 @@ int main(int argc, char **argv){
 
     char* endptr;
     uint16_t port = (uint16_t)strtoul(argv[1], &endptr, 10);
-    printf("%s\n%d\n", argv[1], port);
 
 
     node = node_create(port, argv[2]);
-    if (node){
-        printf("gotnode\n");
+    if (!node){
+        return -1;
     }
     net = node_get_net(node);
-    if (net){
-        printf("gotnet\n");
+    if (!net){
+        return -1;
     }
     node_set_node_msg_handler(node, read_in_msg, NULL);
 
 
     if (argc > 4){
         unsigned char buf[sizeof(struct in_addr)];
-        int rc = inet_pton(AF_INET, argv[3], buf);
+        inet_pton(AF_INET, argv[3], buf);
         uint32_t ip = 0;
         for(int i = 0; i < 4; i++){
             ip += (buf[i] << (8* (3 - i)));
@@ -150,7 +131,6 @@ int main(int argc, char **argv){
         struct node_info ninfo;
         ninfo.IP = ip;
         ninfo.port = port;
-        printf("%08X:%d\n", ip, port);
         node_network_join(node, ninfo, net_joined, NULL);
     }else{
         node_network_create(node, net_joined, NULL);
